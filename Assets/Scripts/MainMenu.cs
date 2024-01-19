3e40b32b8c4dc56dca0301c09e951c60676a5dcb
Tomsoz125 <razertomsoz@gmail.com>
2024-01-19 19:51:52 +0000

    Main menu now finally fully works

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,72 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    public GameObject levelSelector;
+
+    private string playerName;
+
+    public void SelectLevel(GameObject textInput) {
+        if (playerName == null || playerName == "") {
+            int placeholderId = 0;
+            for (int i = 0; i < textInput.transform.GetChild(0).childCount; i++) {
+                if (textInput.transform.GetChild(0).GetChild(i).name == "Placeholder") {
+                    placeholderId = i;
+                    break;
+                }
+            }
+            textInput.transform.GetChild(0).GetChild(placeholderId).GetComponent<TMP_Text>().text = "You must enter a name!";
+            return;
+        }
+
+        gameObject.SetActive(false);
+        levelSelector.SetActive(true);
     }
 
     public void QuitGame() {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void SelectName(GameObject nameObject) {
+        int placeholderId = 0;
+        for (int i = 0; i < nameObject.transform.GetChild(0).childCount; i++) {
+            if (nameObject.transform.GetChild(0).GetChild(i).name == "Placeholder") {
+                placeholderId = i;
+                break;
+            }
+        }
+        nameObject.transform.GetChild(0).GetChild(placeholderId).gameObject.SetActive(false);
+    }
+
+    public void DeselectName(GameObject nameObject) {
+        if (nameObject.transform.GetComponent<TMP_InputField>().text == null || nameObject.transform.GetComponent<TMP_InputField>().text == "") {
+            int placeholderId = 0;
+            for (int i = 0; i < nameObject.transform.GetChild(0).childCount; i++) {
+                if (nameObject.transform.GetChild(0).GetChild(i).name == "Placeholder") {
+                    placeholderId = i;
+                    break;
+                }
+            }
+            nameObject.transform.GetChild(0).GetChild(placeholderId).gameObject.SetActive(true);
+        }
+    }
+
+    public void ChangeName(GameObject inputField) {
+        TMP_InputField input = inputField.GetComponent<TMP_InputField>();
+        if (input == null) {
+            Debug.LogError("Couldn't find input field component on name input!");
+            return;
+        }
+
+        playerName = input.text;
     }
 }
