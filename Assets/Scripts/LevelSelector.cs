364a7a4472d39729a679024368d0165bf4e06b7a
Tomsoz125 <razertomsoz@gmail.com>
2024-01-19 04:54:18 +0000

    Finished the level selector

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,58 @@
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    public GameObject levelObject;
+    public List<GameObject> levelButtons = new List<GameObject>();
+    public Dictionary<int, int> scenes = new Dictionary<int, int>();
+
+    void Awake() {
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++) {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            string[] scenePathSplit = scenePath.Split("/");
+            string name = scenePathSplit[scenePathSplit.Length - 1].Split(".")[0];
+
+            if (!name.Contains("Level")) continue;
+
+            int levelNo = int.Parse(name.Replace("Level", ""));
+
+            Vector2 position;
+            if (levelButtons.Count == 0) {
+                position = new Vector2(-250, 150);
+            } else {
+                position = levelButtons.Last().transform.position;
+                if (position.x >= 250) {
+                    position.y -= 200;
+                    position.x = -250;
+                } else {
+                    position.x += 250;
+                }
+            }
+
+            GameObject levelObj = Instantiate(levelObject);
+            levelObj.transform.SetParent(transform, false);
+            levelObj.transform.localPosition = position;
+            levelObj.name = "Level" + levelNo;
+            Button buttonObj = levelObj.GetComponent<Button>();
+            if (buttonObj == null) {
+                Destroy(levelObj);
+                Debug.LogError("Level object " + levelNo + " doesn't have a button component on it and was removed!");
+                continue;
+            }
+
+            buttonObj.onClick.AddListener(delegate {SelectLevel(levelObj);});
+
+            scenes.Add(levelNo, i);
+        }
+    }
+
+    public void SelectLevel(GameObject btn) {
+        int levelNo = int.Parse(btn.name.Replace("Level", ""));
+        int sceneNo = scenes.GetValueOrDefault(levelNo, 1);
+        SceneManager.LoadScene(sceneNo);
+    }
+}
